d9f3c82d8f592710a613d68906aa1c6b3793219f
GabrielaVDD <vdoviak.gabriela@gmail.com>
2021-11-21 22:36:24 +0200

    4lab

---
+++ Lab4.m
@@ -0,0 +1,214 @@
+%% IS LD3
+% Atliko: DISfm-21 gr. stud. Gabriela Vdoviak
+
+% 1. Paruosti duomenys a) mokymui b) testavimui
+% 2. 
+
+% pasirasyti ant lapo pvz savo varda, kad nebutu trukiu ir kad butu ryskiai
+% uzrasyta. (failas, pavyzdziu eiluciu skaicius)
+% eye(11)eye(11).... -> 11 istrizaines skaicius
+clc;
+clear all;
+close all;
+x = pozymiai_raidems_atpazinti('pic1.jpg',5);
+
+%% read the image with hand-written characters
+pavadinimas = 'pic1.jpg';
+pozymiai_tinklo_mokymui = pozymiai_raidems_atpazinti(pavadinimas, 5);
+%% Atpaþintuvo kûrimas
+%% Development of character recognizer
+% poþymiai ið celiø masyvo perkeliami á matricà
+% take the features from cell-type variable and save into a matrix-type variable
+P = cell2mat(pozymiai_tinklo_mokymui);
+% sukuriama teisingø atsakymø matrica: 11 raidþiø, 8 eilutës mokymui
+% create the matrices of correct answers for each line (number of matrices = number of symbol lines)
+T = [eye(7), eye(7), eye(7), eye(7), eye(7)];
+% sukuriamas SBF tinklas duotiems P ir T sàryðiams
+% create an RBF network for classification with 13 neurons, and sigma = 1
+tinklas = newrb(P,T,0,1,12);
+
+%% Tinklo patikra | Test of the network (recognizer)
+% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
+% estimate output of the network for unknown symbols (row, that were not used during training)
+P2 = P(:,12:22);
+Y2 = sim(tinklas, P2);
+% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
+% find which neural network output gives maximum value
+[a2, b2] = max(Y2);
+%% Rezultato atvaizdavimas
+%% Visualize result
+% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
+% calculate the total number of symbols in the row
+raidziu_sk = size(P2,2);
+% rezultatà saugosime kintamajame 'atsakymas'
+% we will save the result in variable 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+        case 1
+            % the symbol here should be the same as written first symbol in your image
+            atsakymas = [atsakymas, 'S'];
+        case 2
+            atsakymas = [atsakymas, 'I'];
+        case 3
+            atsakymas = [atsakymas, 'S'];
+        case 4
+            atsakymas = [atsakymas, 'T'];
+        case 5
+            atsakymas = [atsakymas, 'E'];
+        case 6
+            atsakymas = [atsakymas, 'M'];
+        case 7
+            atsakymas = [atsakymas, 'A'];
+    end
+end
+% pateikime rezultatà komandiniame lange
+% show the result in command window
+disp(atsakymas)
+% % figure(7), text(0.1,0.5,atsakymas,'FontSize',38)
+%% þodþio "STMA" poþymiø iðskyrimas 
+%% Extract features of the test image
+pavadinimas = 'test2.jpeg';
+pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
+
+%% Raidþiø atpaþinimas
+%% Perform letter/symbol recognition
+% poþymiai ið celiø masyvo perkeliami á matricà
+% features from cell-variable are stored to matrix-variable
+P2 = cell2mat(pozymiai_patikrai);
+% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
+% estimating neuran network output for newly estimated features
+Y2 = sim(tinklas, P2);
+% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
+% searching which output gives maximum value
+[a2, b2] = max(Y2);
+%% Rezultato atvaizdavimas | Visualization of result
+% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
+% calculating number of symbols - number of columns
+raidziu_sk = size(P2,2);
+% rezultatà saugosime kintamajame 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+         case 1
+            % the symbol here should be the same as written first symbol in your image
+            atsakymas = [atsakymas, 'S'];
+        case 2
+            atsakymas = [atsakymas, 'I'];
+        case 3
+            atsakymas = [atsakymas, 'S'];
+        case 4
+            atsakymas = [atsakymas, 'T'];
+        case 5
+            atsakymas = [atsakymas, 'E'];
+        case 6
+            atsakymas = [atsakymas, 'M'];
+        case 7
+            atsakymas = [atsakymas, 'A'];
+    end
+end
+% pateikime rezultatà komandiniame lange
+disp(atsakymas)
+figure(8), text(0.1,0.5,atsakymas,'FontSize',38), axis off
+
+
+%% PAPILDOMA UŽDUOTIS
+%% read the image with hand-written characters
+pavadinimas = 'pic1.jpg';
+pozymiai_tinklo_mokymui = pozymiai_raidems_atpazinti(pavadinimas, 5);
+%% Atpaþintuvo kûrimas
+%% Development of character recognizer
+% poþymiai ið celiø masyvo perkeliami á matricà
+% take the features from cell-type variable and save into a matrix-type variable
+P = cell2mat(pozymiai_tinklo_mokymui);
+% sukuriama teisingø atsakymø matrica: 11 raidþiø, 8 eilutës mokymui
+% create the matrices of correct answers for each line (number of matrices = number of symbol lines)
+T = [eye(7), eye(7), eye(7), eye(7), eye(7)];
+% sukuriamas SBF tinklas duotiems P ir T sàryðiams
+% create an RBF network for classification with 13 neurons, and sigma = 1
+net = feedforwardnet(12);
+tinklas = train(net,P,T);
+
+%% Tinklo patikra | Test of the network (recognizer)
+% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
+% estimate output of the network for unknown symbols (row, that were not used during training)
+P2 = P(:,12:22);
+Y2 = sim(tinklas, P2);
+% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
+% find which neural network output gives maximum value
+[a2, b2] = max(Y2);
+%% Rezultato atvaizdavimas
+%% Visualize result
+% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
+% calculate the total number of symbols in the row
+raidziu_sk = size(P2,2);
+% rezultatà saugosime kintamajame 'atsakymas'
+% we will save the result in variable 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+        case 1
+            % the symbol here should be the same as written first symbol in your image
+            atsakymas = [atsakymas, 'S'];
+        case 2
+            atsakymas = [atsakymas, 'I'];
+        case 3
+            atsakymas = [atsakymas, 'S'];
+        case 4
+            atsakymas = [atsakymas, 'T'];
+        case 5
+            atsakymas = [atsakymas, 'E'];
+        case 6
+            atsakymas = [atsakymas, 'M'];
+        case 7
+            atsakymas = [atsakymas, 'A'];
+    end
+end
+% pateikime rezultatà komandiniame lange
+% show the result in command window
+disp(atsakymas)
+% % figure(7), text(0.1,0.5,atsakymas,'FontSize',38)
+%% þodþio "STMA" poþymiø iðskyrimas 
+%% Extract features of the test image
+pavadinimas = 'test2.jpeg';
+pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
+
+%% Raidþiø atpaþinimas
+%% Perform letter/symbol recognition
+% poþymiai ið celiø masyvo perkeliami á matricà
+% features from cell-variable are stored to matrix-variable
+P2 = cell2mat(pozymiai_patikrai);
+% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
+% estimating neuran network output for newly estimated features
+Y2 = sim(tinklas, P2);
+% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
+% searching which output gives maximum value
+[a2, b2] = max(Y2);
+%% Rezultato atvaizdavimas | Visualization of result
+% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
+% calculating number of symbols - number of columns
+raidziu_sk = size(P2,2);
+% rezultatà saugosime kintamajame 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+         case 1
+            % the symbol here should be the same as written first symbol in your image
+            atsakymas = [atsakymas, 'S'];
+        case 2
+            atsakymas = [atsakymas, 'I'];
+        case 3
+            atsakymas = [atsakymas, 'S'];
+        case 4
+            atsakymas = [atsakymas, 'T'];
+        case 5
+            atsakymas = [atsakymas, 'E'];
+        case 6
+            atsakymas = [atsakymas, 'M'];
+        case 7
+            atsakymas = [atsakymas, 'A'];
+    end
+end
+% pateikime rezultatà komandiniame lange
+disp(atsakymas)
+figure(10), text(0.1,0.5,atsakymas,'FontSize',38), axis off
